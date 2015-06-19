03a36864542ddebbe3cd32d7d05dc9bf73c43eb3
Fabio Ros <fabio.ros90@gmail.com>
2015-06-19 16:15:46 +0200

    Front-end model updated

--- prototipi/FRONTEND/PREMI-FRONTEND-NG/js/model.js
+++ prototipi/FRONTEND/PREMI-FRONTEND-NG/js/model.js
@@ -10,7 +10,7 @@ function Component(x, y, zIndex, width, height, angle, opacity) {
 }
 
 function Text(x, y, zIndex, width, height, angle, opacity, string, size, color, font, bold, italic) {
-    Component.call(this, x, y, zIndex, width, height, angle, opacity);
+    Component.call(this, x, y, zIndex, width, height, angle, opacity);//costruttore di Component con i parametri successivi a this
     /*this = new Component();*/
     this.string = string;
     this.size = size;
@@ -19,12 +19,54 @@ function Text(x, y, zIndex, width, height, angle, opacity, string, size, color, 
     this.color = bold;
     this.font = italic;
 }
-Text.prototype = new Component();
 
-/* DOVREBBE ESERE GIUSTO CON QUELLI QUI SOTTO*/
-Text.prototype.string;
-Text.prototype.size;
-Text.prototype.color;
-Text.prototype.font;
-Text.prototype.color;
-Text.prototype.font;
+function Chart(x, y, zIndex, width, height, angle, opacity, type, data, options) {
+    Component.call(this, x, y, zIndex, width, height, angle, opacity);
+    this.type = type;
+    this.data = data;
+    this.options = options;
+}
+
+function Image(x, y, zIndex, width, height, angle, opacity, path) {
+    Component.call(this, x, y, zIndex, width, height, angle, opacity);
+    this.path = path;
+}
+
+function Table(x, y, zIndex, width, height, angle, opacity, row, column, title, data) {
+    Component.call(this, x, y, zIndex, width, height, angle, opacity);
+    this.row = row;
+    this.column= column;
+    this.title = title;
+    this.data=data; //elements of the table, they are components, usually Text
+}
+
+function RealTimeData(x, y, zIndex, width, height, angle, opacity, pathParser, pathHandler, pathFallback) {
+    Component.call(this, x, y, zIndex, width, height, angle, opacity);
+    this.pathParser = pathParser;
+    this.pathHandler= pathHandler;
+    this.pathFallback = pathFallback;
+}
+
+function Slide(x,y, id, convertedImagePath){
+    this.id = id;    
+    this.x = x;
+    this.y = y;
+    this.convertedImagePath=convertedImagePath;
+};
+
+function Presentation(id,projectId){
+    this.id = id;    
+    this.x = x;
+}
+
+function Infographic(id,path){
+    this.id = id;    
+    this.path = path;
+}
+
+
+function Project(id, ownerId){
+    this.id = id;    
+    this.ownerId = ownerId;
+}
+
